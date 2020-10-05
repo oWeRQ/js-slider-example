aec939cb89cb2ae6a68a174bd6b0e9806dca82d4
Maxim <fomaxi@gmail.com>
2020-10-05 23:44:07 +0300

    fix positions

--- slider.js
+++ slider.js
@@ -13,6 +13,8 @@ function Slider(options) {
 	this.nearCount = Math.floor(this.visibleCount / 2);
 	this.position = 0;
 
+	this.itemPositions = Array.from(this.items).map((item, i) => i);
+
 	this.arrowLeft.addEventListener('click', e => {
 		e.preventDefault();
 		this.slideTo(this.position - 1);
@@ -37,32 +39,43 @@ Slider.prototype.getIndex = function(position) {
 	return ((position % this.items.length) + this.items.length) % this.items.length;
 }
 
-Slider.prototype.getItem = function(position) {
-	return this.items[this.getIndex(position)];
+Slider.prototype.getOffset = function(position) {
+	return position - this.getIndex(position);
 }
 
-Slider.prototype.updateItemPosition = function(position) {
-	this.getItem(position).style.transform = 'translateX(' + ((position - this.getIndex(position)) * 100) + '%)';
+Slider.prototype.getItem = function(position) {
+	return this.items[this.getIndex(position)];
 }
 
 Slider.prototype.updateWrapperPosition = function(position) {
 	this.wrapper.style.transform = 'translateX(' + (-position / this.visibleCount * 100) + '%)';
 }
 
-Slider.prototype.slideTo = function(nextPosition, silent) {
-	for (var i = nextPosition - this.nearCount; i <= nextPosition + this.nearCount; i++) {
+Slider.prototype.updateItemPosition = function(position) {
+	this.getItem(position).style.transform = 'translateX(' + (this.getOffset(position) * 100) + '%)';
+	this.itemPositions[this.getIndex(position)] = position;
+}
+
+Slider.prototype.getItemPosition = function(position) {
+	return this.itemPositions[this.getIndex(position)];
+}
+
+Slider.prototype.slideTo = function(position, silent) {
+	position = this.getItemPosition(position);
+
+	for (var i = position - this.nearCount; i <= position + this.nearCount; i++) {
 		this.updateItemPosition(i);
 	}
 
-	this.updateWrapperPosition(nextPosition - this.nearCount);
+	this.updateWrapperPosition(position - this.nearCount);
 
 	this.getItem(this.position).classList.remove('m-active');
-	this.getItem(nextPosition).classList.add('m-active');
+	this.getItem(position).classList.add('m-active');
 
 	if (!silent) {
-		this.slideChange(nextPosition, this.position);
+		this.slideChange(position, this.position);
 	}
-	this.position = nextPosition;
+	this.position = position;
 }
 
 var previewSlider = new Slider({
