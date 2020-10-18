870a8d9ed8c2f9e0ac8dcd697cbb409a2ff25fe0
Maxim <fomaxi@gmail.com>
2020-10-18 15:45:27 +0300

    cleanup

--- slider.js
+++ slider.js
@@ -3,83 +3,98 @@ class Slider {
 		this.el = typeof options.el === 'string' ? document.querySelector(options.el) : options.el;
 		this.slideChange = options.slideChange;
 		this.activeClass = options.activeClass || 'm-active';
+		this.wrapperClass = options.wrapperClass || '.b-slider-wrapper';
+		this.itemClass = options.itemClass || '.b-slider-item';
+		this.prevClass = options.prevClass || '.b-slider-arrow_left';
+		this.nextClass = options.nextClass || '.b-slider-arrow_right';
 		this.touchThreshold = +options.touchThreshold || 50;
 		this.position = +options.position || 0;
 
 		this.init();
 	}
+
 	init() {
 		this.initElements();
-		this.initListeners();
+		this.initClick();
+		this.initTouch();
 		this.resetOffset();
-		this.updateActive(this.position, true)
 	}
+
 	destroy() {
-		this.destroyListeners();
-		this.updateActive(this.position, false)
+		this.destroyClick();
+		this.destroyTouch();
 	}
+
 	initElements() {
-		this.wrapper = this.el.querySelector('.b-slider-wrapper');
-		this.items = Array.from(this.el.querySelectorAll('.b-slider-item'));
-		this.arrowLeft = this.el.querySelector('.b-slider-arrow_left');
-		this.arrowRight = this.el.querySelector('.b-slider-arrow_right');
+		this.wrapper = this.el.querySelector(this.wrapperClass);
+		this.items = Array.from(this.el.querySelectorAll(this.itemClass));
+		this.prev = this.el.querySelector(this.prevClass);
+		this.next = this.el.querySelector(this.nextClass);
 
 		this.transitionDuration = parseFloat(getComputedStyle(this.wrapper).transitionDuration) * 1000;
-
 		this.wrapperWidth = this.wrapper.clientWidth;
 		this.itemWidth = this.items[0].clientWidth;
 		this.visibleCount = this.wrapperWidth / this.itemWidth;
 		this.nearCount = Math.floor(this.visibleCount / 2);
 	}
-	initListeners() {
-		this.arrowLeft.addEventListener('click', this.arrowLeftHandler = e => {
+
+	initClick() {
+		this.prev.addEventListener('click', this.prevListener = e => {
 			e.preventDefault();
 			this.slideToEmit(this.position - 1);
 		});
-
-		this.arrowRight.addEventListener('click', this.arrowRightHandler = e => {
+		this.next.addEventListener('click', this.nextListener = e => {
 			e.preventDefault();
 			this.slideToEmit(this.position + 1);
 		});
-
-		this.itemsHandlers = this.items.map((el, index) => {
-			const handler = e => {
+		this.itemsListeners = this.items.map((el, index) => {
+			const listener = e => {
 				e.preventDefault();
 				this.slideToEmit(index);
 			};
-			el.addEventListener('click', handler);
-			return handler;
+			el.addEventListener('click', listener);
+			return listener;
 		});
+	}
 
+	destroyClick() {
+		this.prev.removeEventListener('click', this.prevListener);
+		this.next.removeEventListener('click', this.nextListener);
+		this.itemsListeners.forEach((listener, i) => {
+			this.items[i].removeEventListener('click', listener);
+		});
+	}
+
+	initTouch() {
 		let touchStartX;
-		this.el.addEventListener('touchstart', this.touchstartHandler = e => {
+		this.el.addEventListener('touchstart', this.touchstartListener = e => {
 			touchStartX = e.changedTouches[0].clientX;
 		});
-		this.el.addEventListener('touchend', this.touchendHandler = e => {
+		this.el.addEventListener('touchend', this.touchendListener = e => {
 			const deltaX = e.changedTouches[0].clientX - touchStartX;
 			if (Math.abs(deltaX) > this.touchThreshold) {
 				this.slideToEmit(this.position - Math.sign(deltaX));
 			}
 		});
 	}
-	destroyListeners() {
-		this.arrowLeft.removeEventListener('click', this.arrowLeftHandler);
-		this.arrowRight.removeEventListener('click', this.arrowRightHandler);
-		this.itemsHandlers.forEach((handler, i) => {
-			this.items[i].removeEventListener('click', handler);
-		});
-		this.el.removeEventListener('touchstart', this.touchstartHandler);
-		this.el.removeEventListener('touchend', this.touchendHandler);
+
+	destroyTouch() {
+		this.el.removeEventListener('touchstart', this.touchstartListener);
+		this.el.removeEventListener('touchend', this.touchendListener);
 	}
+
 	getOffset(position) {
 		return Math.floor(position / this.items.length) * this.items.length;
 	}
+
 	getIndex(position) {
 		return position - this.getOffset(position);
 	}
+
 	getItem(position) {
 		return this.items[this.getIndex(position)];
 	}
+
 	getItemPosition(position) {
 		const diff = this.getIndex(position) - this.getIndex(this.position);
 		const back = (diff < 0 ? 0 : this.items.length) - diff;
@@ -87,15 +102,19 @@ class Slider {
 
 		return this.position + (back < forward ? -back : forward);
 	}
+
 	updateWrapperPosition(position) {
 		this.wrapper.style.transform = 'translateX(' + (-position / this.visibleCount * 100) + '%)';
 	}
+
 	updateItemPosition(position) {
 		this.getItem(position).style.transform = 'translateX(' + (this.getOffset(position) * 100) + '%)';
 	}
+
 	updateActive(position, state) {
 		this.getItem(position).classList.toggle(this.activeClass, state);
 	}
+
 	setPosition(position) {
 		for (var i = position - this.nearCount; i <= position + this.nearCount; i++) {
 			this.updateItemPosition(i);
@@ -103,23 +122,26 @@ class Slider {
 
 		this.updateWrapperPosition(position - this.nearCount);
 
+		this.updateActive(this.position, false);
+		this.updateActive(position, true);
+
 		this.position = position;
 	}
+
 	resetOffset() {
 		this.wrapper.style.transitionDuration = '0s';
 		this.setPosition(this.getIndex(this.position));
 		void(this.wrapper.clientWidth); // force reflow
 		this.wrapper.style.transitionDuration = '';
 	}
-	slideTo(position) {
-		this.updateActive(this.position, false);
-		this.updateActive(position, true);
 
+	slideTo(position) {
 		this.setPosition(this.getItemPosition(position));
 
 		clearTimeout(this.resetOffsetTimeout);
 		this.resetOffsetTimeout = setTimeout(() => this.resetOffset(), this.transitionDuration);
 	}
+
 	slideToEmit(position) {
 		this.slideTo(position);
 		this.slideChange && this.slideChange(this.position);
